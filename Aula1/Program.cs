c1c39b3c10f5274e2ec768e0b92f687cac053d57
eichild <44980461+eichild@users.noreply.github.com>
2019-09-13 09:35:28 -0300

    Arquivos incluidos

---
+++ Aula1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando variavel
+            int num1;
+
+            //Declarando atribuindo um valor
+            int num2 = 5;
+
+            //Declarando diversas variáveis de mesmo tipo
+            int num4 = 7;
+
+            //Declarando variavel decimal
+            float media;
+
+            //Variavel do tipo lógico
+            bool resultado = false;
+            
+            Console.WriteLine(num2+num4);
+        }
+    }
+}
