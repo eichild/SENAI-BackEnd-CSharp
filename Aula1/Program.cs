e10d7a256b7662327690ee75f526092fc3bb0dff
eichild <44980461+eichild@users.noreply.github.com>
2019-09-13 11:13:30 -0300

    Primeiro programa rodando

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -6,22 +6,36 @@ namespace Aula1
     {
         static void Main(string[] args)
         {
-            //Declarando variavel
             int num1;
-
-            //Declarando atribuindo um valor
-            int num2 = 5;
-
-            //Declarando diversas variáveis de mesmo tipo
-            int num4 = 7;
-
-            //Declarando variavel decimal
+            int num2;
+            int num3;
             float media;
-
-            //Variavel do tipo lógico
             bool resultado = false;
+            string nomeAluno;
+
+            Console.WriteLine("\nBem vindos à nossa primeira aplicação");
+
+            Console.Write("Digite");
+            Console.Write(" seu nome ");
             
-            Console.WriteLine(num2+num4);
+           nomeAluno= Console.ReadLine();
+           Console.WriteLine(nomeAluno + ", Agora digite sua primeira nota: ");
+            
+            //Convertendo numero inteiro para ser lido
+            num1 = int.Parse(Console.ReadLine());
+
+            //Interpolação
+            Console.WriteLine($"Sua primeira nota é {num1}");
+
+            Console.WriteLine("Digite sua segunda nota");
+            num2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite sua terceira nota");
+            num3 =  int.Parse(Console.ReadLine());
+
+            media=(num1+num2+num3)/3;
+
+            Console.WriteLine("Sua média é : " +media);
         }
     }
 }
