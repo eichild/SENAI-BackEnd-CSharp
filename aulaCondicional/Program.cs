ba340b72cd8b97ac40d09dac9a89599d07522a5e
eichild <44980461+eichild@users.noreply.github.com>
2019-09-16 11:51:32 -0300

    Aula sobre condicional

---
+++ aulaCondicional/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace aulaCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media;
+
+            Console.Write("Digite a primeira nota: ");
+            nota1=double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            nota2=double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a primeira nota: ");
+            nota3=double.Parse(Console.ReadLine());
+
+            media=(nota1+nota2+nota3)/3;
+
+            //IF ENCADEADO
+            if(media>=6){
+                Console.WriteLine("Aprovado");
+            }
+            else if(media<6 && media>=4){
+                Console.WriteLine("Recuperação");
+            }
+            else{
+                Console.WriteLine("Reprovado");
+            }
+            }
+        }
+    }
+
