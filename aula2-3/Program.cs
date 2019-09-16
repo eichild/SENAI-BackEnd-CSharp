43695091558543b997404b2202f188b87180b35c
eichild <44980461+eichild@users.noreply.github.com>
2019-09-16 14:14:46 -0300

    Aula sobre repetição e bool

---
+++ aula2-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace aula2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool entrada = false; //Definindo para não utilizar o else no if
+            int idade;
+            string retorno;
+            
+            Console.Write("Digite sua idade: ");
+            idade=int.Parse((Console.ReadLine()));
+
+            if(idade>=18){
+                entrada = true;
+                 retorno = "Entrada Permitida!!!";
+            }
+            //Não precisa desse if
+            if(entrada == true){
+                retorno = "Entrada Permitida!!!";
+            }
+            else{
+                retorno = "Entrada não permitida :c";
+            }
+            Console.Write(retorno);
+        }
+    }
+}
