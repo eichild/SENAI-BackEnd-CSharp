260d2f82db544f356cc03b12b9a6288ebe729e54
eichild <44980461+eichild@users.noreply.github.com>
2019-09-20 12:29:29 -0300

    Aula sobre vetor

---
+++ aula4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aula sobre vetor
+            double[] nota= new double[10];
+
+            for(int i=0; i<=9;i++){
+                Console.Write("Digite nota: ");
+                nota[i]=double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(nota[3]+"\n");
+
+            //Conta quantos indicies tem no vetor
+            for(int i=0; i<nota.Length ;i++){
+                Console.WriteLine("Nota: " +nota[i]);
+            }
+        }
+    }
+}
