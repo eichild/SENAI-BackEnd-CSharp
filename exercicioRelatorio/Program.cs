e9a1032c735d00b71fdb59f02237a2354daa1229
eichild <44980461+eichild@users.noreply.github.com>
2019-09-25 16:00:59 -0300

    Adicionado paragrafo para texto

--- exercicioRelatorio/Program.cs
+++ exercicioRelatorio/Program.cs
@@ -34,8 +34,18 @@ namespace exercicioRelatorio
                 estilo01.Name="Cor do titulo";
                 estilo01.CharacterFormat.TextColor=Color.Pink;
                 relatorio.Styles.Add(estilo01);
-                
+                estilo01.CharacterFormat.FontSize=30;
            #endregion
+
+           #region Adicionando paragrafo para o texto
+               Paragraph texto= secaoConteudo1.AddParagraph();
+           #endregion
+
+            #region Texto do paragrafo do texto
+               texto.AppendText("\tHoje eu achei minha prometida no LINKDIN, Linda linda linda\n");
+           #endregion
+
+
         }
     }
 }
